0ee75db762f04ef46c2a8789a92d063516071bec
ouhai <ouhai@fenbi.com>
2021-06-29 20:56:34 +0800

    ADD:第三方bean，datasource

--- src/main/java/com/example/ouhai/configuration/OuhaiConfig.java
+++ src/main/java/com/example/ouhai/configuration/OuhaiConfig.java
@@ -1,9 +1,31 @@
 package com.example.ouhai.configuration;
 
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan
 public class OuhaiConfig {
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        Properties props = new Properties();
+        props.setProperty("driver", "com.mysql.jdbc.Driver");
+        props.setProperty("url", "jdbc:mysql://localhost:3306");
+        props.setProperty("username","root");
+        props.setProperty("password","196082511");
+        DataSource dataSource = null;
+        try {
+            dataSource = BasicDataSourceFactory.createDataSource(props);
+        } catch (Exception e) {
+             e.printStackTrace();
+        }
+        return dataSource;
+    }
+
 }
